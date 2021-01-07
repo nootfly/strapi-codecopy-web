d4ad5694559520e85a815f2b409f774a0338b797
Noot <nootfly@users.noreply.github.com>
2021-01-07 14:11:10 +1100

    Update gatsby-config.js

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.API_URL || "https://codecopy.herokuapp.com",
         contentTypes: ["article", "category", "writer"],
         singleTypes: [`homepage`, `global`],
         queryLimit: 1000,
